fc166de10f2d51a69be9444f624dfacffb9f46ad
kusrohit <kusrohit@users.noreply.github.com>
2024-05-24 12:16:30 +0530

    modified:   index.html 	modified:   scripts/app.js 	modified:   style/style.css

--- scripts/app.js
+++ scripts/app.js
@@ -6,20 +6,50 @@ const skills_bars = document.querySelectorAll(".skill-progress");
 const records_wrap = document.querySelector(".records");
 const records_numbers = document.querySelectorAll(".number");
 
+const footer_input = document.querySelector(".footer-input");
+
+const hamburger_menu = document.querySelector(".hamburger-menu");
+
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
+
+footer_input.addEventListener("focus", () => {
+  footer_input.classList.add("focus");
+});
+
+footer_input.addEventListener("blur", () => {
+  if (footer_input.value != "") return;
+  footer_input.classList.remove("focus");
+});
+
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
+}
+
+hamburger_menu.addEventListener("click", () => {
+  if (!navbar.classList.contains("open")) {
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scrolling");
+  } else {
+    closeMenu();
+  }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
+
 filter_btns.forEach((btn) =>
   btn.addEventListener("click", () => {
     filter_btns.forEach((button) => button.classList.remove("active"));
     btn.classList.add("active");
 
     let filterValue = btn.dataset.filter;
-    console.log(filterValue);
 
     $(".grid").isotope({ filter: filterValue });
   })
 );
 
 $(".grid").isotope({
-  // options
   itemSelector: ".grid-item",
   layoutMode: "fitRows",
   transitionDuration: "0.6s",
@@ -29,25 +59,20 @@ window.addEventListener("scroll", () => {
   skillsEffect();
   countUp();
 });
-// ckeckScroll(skills_wrap)
 
-function ckeckScroll(elem) {
-  let rect = elem.getBoundingClientRect();
-
-  if (window.innerHeight >= rect.top + elem.offsetHeight) return true;
+function checkScroll(el) {
+  let rect = el.getBoundingClientRect();
+  if (window.innerHeight >= rect.top + el.offsetHeight) return true;
   return false;
 }
 
 function skillsEffect() {
-  if (!ckeckScroll(skills_wrap)) return;
-
-  skills_bars.forEach((skill) => {
-    skill.style.width = skill.dataset.progress;
-  });
+  if (!checkScroll(skills_wrap)) return;
+  skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
 }
 
 function countUp() {
-  if (!ckeckScroll(records_wrap)) return;
+  if (!checkScroll(records_wrap)) return;
   records_numbers.forEach((numb) => {
     const updateCount = () => {
       let currentNum = +numb.innerText;
@@ -57,7 +82,9 @@ function countUp() {
 
       if (currentNum < maxNum) {
         numb.innerText = currentNum + increment;
-        setTimeout(updateCount, 10);
+        setTimeout(updateCount, 1);
+      } else {
+        numb.innerText = maxNum;
       }
     };
 
@@ -65,16 +92,15 @@ function countUp() {
   });
 }
 
-const swiper = new Swiper(".swiper", {
+var mySwiper = new Swiper(".swiper-container", {
   speed: 1100,
   slidesPerView: 1,
   loop: true,
   autoplay: {
-    delay: 2000,
+    delay: 5000,
   },
-  // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
   },
 });
